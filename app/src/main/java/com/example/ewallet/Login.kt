d3745498251ee80dbaf05f49f9417d7bd9dd66d0
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-04-28 11:10:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ewallet/Login.kt
@@ -0,0 +1,169 @@
+package com.example.ewallet
+
+
+import android.os.Bundle
+import android.view.WindowInsetsAnimation
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.toColorInt
+import com.example.ewallet.ui.theme.EWalletTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    val ubuntuFont = FontFamily(
+        Font(R.font.ubuntu_font)
+    )
+
+    var wrongInfo by remember { mutableStateOf(false) }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+
+
+    Column(
+        modifier
+            .fillMaxSize()
+    ) {
+        Column (
+            modifier
+                .fillMaxWidth()
+                .weight(0.5f)
+                .background(color = Color(0xFFCD0000)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = stringResource(id = R.string.welcoming), fontSize = 25.sp, color = Color.White, fontFamily = ubuntuFont)
+        }
+        Spacer(modifier = modifier.height(50.dp))
+        Column (
+            modifier
+                .fillMaxWidth()
+                .weight(2f)
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            if(wrongInfo) {
+                Text(text = "There is no user with such credentials!", fontSize = 15.sp, fontFamily = ubuntuFont, color = Color(0xFFCD0000))
+            }
+            Spacer(modifier = modifier.height(35.dp))
+            Column (
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Email:",
+                    fontSize = 15.sp,
+                    fontFamily = ubuntuFont,
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.padding(5.dp),
+                )
+                TextField(
+                    value = email,
+                    placeholder = { Text("example@email.com") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down)}),
+                    singleLine = true,
+                    onValueChange = {email = it},
+                    modifier = modifier
+                        .clip(RoundedCornerShape(20.dp))
+                        .height(50.dp)
+                        .width(300.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        textColor = Color.Black,
+                        placeholderColor = Color.Gray,
+                        focusedIndicatorColor = Color.Transparent, // removes underline when focused
+                        unfocusedIndicatorColor = Color.Transparent, // removes underline when unfocused
+                        disabledIndicatorColor = Color.Transparent // removes underline when disabled
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Column (
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = "Password:",
+                    fontSize = 15.sp,
+                    fontFamily = ubuntuFont,
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.padding(5.dp),
+                )
+                TextField(
+                    value = password,
+                    placeholder = { Text("Password") },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(onNext = { focusManager.clearFocus()}),
+                    singleLine = true,
+                    onValueChange = {password = it},
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = modifier
+                        .clip(RoundedCornerShape(20.dp))
+                        .height(50.dp)
+                        .width(300.dp),
+                    colors = TextFieldDefaults.textFieldColors(
+                        textColor = Color.Black,
+                        placeholderColor = Color.Gray,
+                        focusedIndicatorColor = Color.Transparent, // removes underline when focused
+                        unfocusedIndicatorColor = Color.Transparent, // removes underline when unfocused
+                        disabledIndicatorColor = Color.Transparent // removes underline when disabled
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Row {
+                Button(
+                    onClick = { wrongInfo = true },
+                    shape = CutCornerShape(10),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFCD0000))
+                ) {
+                    Text("Log in", fontSize = 20.sp, fontFamily = ubuntuFont, color = Color.White)
+                }
+                Spacer(modifier = modifier.width(10.dp))
+                Button(
+                    onClick = { },
+                    shape = CutCornerShape(10),
+                    border = BorderStroke(1.dp, Color.Red),
+                    colors = ButtonDefaults.outlinedButtonColors(contentColor = Color(0xFFCD0000))
+                ) {
+                    Text("Register", fontSize = 20.sp, fontFamily = ubuntuFont, color = Color(0xFFCD0000))
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ewallet/MainMenu.kt
@@ -0,0 +1,78 @@
+package com.example.ewallet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val ubuntuFont = FontFamily(
+    Font(R.font.ubuntu_font)
+)
+
+@Composable
+fun MainMenuScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier
+                .fillMaxWidth()
+                .weight(0.5f)
+                .background(color = Color(0xFFCD0000)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Welcome: <FULL NAME>",
+                fontSize = 25.sp,
+                color = Color.White,
+                fontFamily = ubuntuFont
+            )
+        }
+        Column(
+            modifier
+                .fillMaxWidth()
+                .weight(2f)
+                .background(Color.White),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            MenuButton(optionName = "My Cards")
+            Spacer(modifier = Modifier.height(16.dp))
+            MenuButton(optionName = "Transaction History")
+            Spacer(modifier = Modifier.height(16.dp))
+            MenuButton(optionName = "New Transaction")
+            Spacer(modifier = Modifier.height(16.dp))
+            MenuButton(optionName = "Monthly Balance Sheet")
+            Spacer(modifier = Modifier.height(16.dp))
+            MenuButton(optionName = "Logout")
+        }
+    }
+}
+
+@Composable
+fun MenuButton(modifier: Modifier = Modifier, optionName: String) {
+    Button(
+        onClick = { },
+        shape = CutCornerShape(10),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFCD0000)),
+        modifier = modifier
+            .width(300.dp)
+            .shadow(10.dp)
+    ) {
+        Text(optionName, fontSize = 20.sp, fontFamily = ubuntuFont, color = Color.White)
+    }
+}
